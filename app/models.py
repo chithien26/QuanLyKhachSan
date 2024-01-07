130f44d221e61eec8592823c82becae447bc889e
Your Name <lechithien26@gmail.com>
2024-01-07 21:58:46 +0700

    update database

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from app import db, app
 from flask_login import UserMixin
 import enum
 from datetime import datetime
+import hashlib
+import json
 
 
 class UserRoleEnum(enum.Enum):
@@ -11,10 +13,10 @@ class UserRoleEnum(enum.Enum):
     ADMIN = 2
 
 
-class ChuVu(db.Model):
-    MaChuVu = Column(Integer, primary_key=True, autoincrement=True)
+class ChucVu(db.Model):
+    MaChucVu = Column(Integer, primary_key=True, autoincrement=True)
     TenChucVu = Column(String(30), nullable=False)
-    NhanVien = relationship('NhanVien', backref='ChucVu', lazy=True)
+    NhanVien = relationship('NhanVien', backref='chucVu', lazy=True)
 
     def __str__(self):
         return self.TenChucVu
@@ -27,8 +29,8 @@ class NhanVien(db.Model):
     NgaySinh = Column(DateTime, nullable=False)
     GioiTinh = Column(String(10), nullable=False)
     NgayVaoLam = Column(DateTime, default=datetime.now())
-    ChucVu = Column(Integer, ForeignKey(ChuVu.MaChuVu), nullable=False)
-    TaiKhoanNV = relationship('TaiKhoanNhanVien', backref='NhanVien', lazy=True)
+    ChucVu = Column(Integer, ForeignKey(ChucVu.MaChucVu), nullable=False)
+    TaiKhoanNV = relationship('TaiKhoanNhanVien', backref='nhanVien', lazy=True)
 
 class TaiKhoanNhanVien(db.Model):
     MaTK = Column(Integer, primary_key=True, autoincrement=True)
@@ -42,7 +44,7 @@ class TaiKhoanNhanVien(db.Model):
 class LoaiKhachHang(db.Model):
     MaLoaiKH = Column(Integer, primary_key=True, autoincrement=True)
     TenLoaiKH = Column(String(20), nullable=False)
-    KhachHang = relationship('KhachHang',backref='LoaiKhachHang', lazy=True)
+    KhachHang = relationship('KhachHang',backref='loaiKhachHang', lazy=True)
 
 class KhachHang(db.Model):
     MaKH = Column(Integer, primary_key=True, autoincrement=True)
@@ -52,9 +54,9 @@ class KhachHang(db.Model):
     CMND = Column(String(20), nullable=False)
     DiaChi = Column(String(50), nullable=True)
     QuocTich = Column(String(20), nullable=False)
-    MaKH = Column(Integer, ForeignKey(LoaiKhachHang.MaLoaiKH), nullable=False)
-    TaiKhoanKH = relationship('TaiKhoanKhachHang', backref='KhachHang', lazy=True)
-    PhieuThuePhong = relationship('PhieuThuePhong', backref='KhachHang', lazy=True)
+    LoaiKH = Column(Integer, ForeignKey(LoaiKhachHang.MaLoaiKH), nullable=False)
+    TaiKhoanKH = relationship('TaiKhoanKhachHang', backref='khachHang', lazy=True)
+    PhieuThuePhong = relationship('PhieuThuePhong', backref='khachHang', lazy=True)
 
 class TaiKhoanKhachHang(db.Model):
     MaTK = Column(Integer, primary_key=True, autoincrement=True)
@@ -69,7 +71,7 @@ class LoaiPhong(db.Model):
     MaLoaiPhong = Column(Integer, primary_key=True, autoincrement=True)
     TenLoaiPhong = Column(String(20), nullable=False)
     DonGia = Column(Float, nullable=False)
-    Phong = relationship('Phong', backref='LoaiPhong', lazy=True)
+    Phong = relationship('Phong', backref='loaiPhong', lazy=True)
 
 class Phong(db.Model):
     MaPhong = Column(Integer, primary_key=True, autoincrement=True)
@@ -77,15 +79,14 @@ class Phong(db.Model):
     SoKhachToiDa = Column(Integer, nullable=True, default=3)
     MoTa = Column(String(100), nullable=True, default='Phòng dành cho 3 khách')
     MaLoaiPhong = Column(Integer, ForeignKey(LoaiPhong.MaLoaiPhong), nullable=False)
-    DonDatPhong = relationship('DonDatPhong', backref='Phong', lazy=True)
-    ChiTietDonDatPhong = relationship('ChiTietDonDatPhong', backref='Phong', lazy=True)
+    DonDatPhong = relationship('DonDatPhong', backref='phong', lazy=True)
+    ChiTietDonDatPhong = relationship('ChiTietDonDatPhong', backref='phong', lazy=True)
 
 class DonDatPhong(db.Model):
     MaDonDatPhong = Column(Integer, primary_key=True, autoincrement=True)
     MaPhong = Column(Integer, ForeignKey(Phong.MaPhong), nullable=False)
     NgayDatPhong = Column(DateTime, default=datetime.now())
-    DaThanhToan = Column(Boolean, default=False)
-    ChiTietDonDatPhong = relationship('ChiTietDonDatPhong', backref='DonDatPhong', lazy=True)
+    ChiTietDonDatPhong = relationship('ChiTietDonDatPhong', backref='donDatPhong', lazy=True)
 
 class ChiTietDonDatPhong(db.Model):
     MaPhong = Column(Integer, ForeignKey(Phong.MaPhong), primary_key=True)
@@ -99,7 +100,7 @@ class PhieuThuePhong(db.Model):
     NgayTraPhong = Column(DateTime, nullable=False)
     SoLuongKhach = Column(Integer, nullable=False)
     KhachHang = Column(Integer, ForeignKey(KhachHang.MaKH), nullable=False)
-    HoaDon = relationship('HoaDon', backref='PhieuThuePhong', lazy=True)
+    HoaDon = relationship('HoaDon', backref='phieuThuePhong', lazy=True)
 
 class HoaDon(db.Model):
     MaHoaDon = Column(Integer, primary_key=True, autoincrement=True)
@@ -107,13 +108,71 @@ class HoaDon(db.Model):
     ThanhTien = Column(Float, nullable=False)
     NgayThanhToan = Column(DateTime, default=datetime.now())
     PhieuThuePhong = Column(Integer, ForeignKey(PhieuThuePhong.MaPhieuThuePhong), nullable=False)
-    ChiTietPhuThu = relationship('ChiTietPhuThu', backref='HoaDon', lazy=True)
+    ChiTietPhuThu = relationship('ChiTietPhuThu', backref='hoaDon', lazy=True)
+    DaThanhToan = Column(Boolean, default=False)
+
 class PhuThu(db.Model):
     MaPhuThu = Column(Integer, primary_key=True, autoincrement=True)
-    TenLoaiTaiPhuThu = Column(String(30), nullable=False)
+    TenLoaiPhuThu = Column(String(30), nullable=False)
     HeSo = Column(Float, nullable=False)
-    ChiTietPhuThu = relationship('ChiTietPhuThu', backref='PhuThu', lazy=True)
+    ChiTietPhuThu = relationship('ChiTietPhuThu', backref='phuThu', lazy=True)
 
 class ChiTietPhuThu(db.Model):
     HoaDon = Column(Integer, ForeignKey(HoaDon.MaHoaDon), primary_key=True)
     PhuThu = Column(Integer, ForeignKey(PhuThu.MaPhuThu), primary_key=True)
+
+def read_json(path):
+    with open(path,'r') as f:
+        return json.load(f)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
+# 1
+#         chucVu = read_json('data/ChucVu.json')
+#         for i in chucVu:
+#             a = ChucVu(TenChucVu=i["TenChucVu"])
+#             db.session.add(a)
+#             db.session.commit()
+#
+#         loaiKhachHang = read_json('data/LoaiKhachHang.json')
+#         for i in loaiKhachHang:
+#             a = LoaiKhachHang(TenLoaiKH=i["TenLoaiKH"])
+#             db.session.add(a)
+#             db.session.commit()
+#
+#         loaiPhong = read_json('data/LoaiPhong.json')
+#         for i in loaiPhong:
+#             a = LoaiPhong(TenLoaiPhong=i["TenLoaiPhong"], DonGia=i["DonGia"])
+#             db.session.add(a)
+#             db.session.commit()
+#
+#         phuThu = read_json('data/PhuThu.json')
+#         for i in phuThu:
+#             a = PhuThu(TenLoaiPhuThu=i["TenLoaiPhuThu"], HeSo=i["HeSo"])
+#             db.session.add(a)
+#             db.session.commit()
+#2
+
+        # khachHang = read_json('data/KhachHang.json')
+        # for i in khachHang:
+        #     a = KhachHang(HoKH=i["HoKH"], TenKH=i["TenKH"], Phone=i["Phone"],
+        #                   CMND=i['CMND'], DiaChi=i['DiaChi'],
+        #                   QuocTich=i["QuocTich"],LoaiKH=i["LoaiKH"])
+        #     db.session.add(a)
+        #     db.session.commit()
+
+        # taiKhoanKhachHang = read_json('data/TaiKhoanKhachHang.json')
+        # for i in taiKhoanKhachHang:
+        #     a = TaiKhoanKhachHang(TenTK=i["TenTK"], Username=i["Username"], Password=i["Password"],
+        #                           Email=i['Email'], Phone=i['Phone'],MaKH=i["MaKH"])
+        #     db.session.add(a)
+        #     db.session.commit()
+
+        # taiKhoanNhanVien = read_json('data/TaiKhoanNhanVien.json')
+        # for i in taiKhoanNhanVien:
+        #     a = TaiKhoanKhachHang(TenTK=i["TenTK"], Username=i["Username"], Password=i["Password"],
+        #                           Email=i['Email'], Phone=i['Phone'], MaNV=i["MaNV"])
+        #     db.session.add(a)
+        #     db.session.commit()
