47a0b7bf9a61672f5911ed94e54cce0a63ebd26a
Your Name <lechithien26@gmail.com>
2024-01-09 22:22:58 +0700

    add background

--- app/static/javascipt/booking.js
+++
@@ -1,6 +0,0 @@
-
-function formatDate() {
-  var inputDate = document.getElementById("checkin_date").value;
-  var formattedDate = new Date(inputDate).toISOString().split('T')[0].replace(/-/g, '/');
-  document.getElementById("checkin_date").value = formattedDate;
-
